53097d37aad39b874351ff52ab0b516353c159e9
Joseph Goodrick <19720019+jgoodrick@users.noreply.github.com>
2022-01-25 10:59:53 -0600

    unit test

--- DemoStoreTests/DemoStoreTests.swift
+++ DemoStoreTests/DemoStoreTests.swift
@@ -1,10 +1,22 @@
 
 import XCTest
 @testable import DemoStore
+import StoreKitTest
 
 class DemoStoreTests: XCTestCase {
+    
+    let productsPlist = Bundle.init(identifier: "com.goodrick.DemoStoreTests")!.url(forResource: "TestProducts", withExtension: "plist")!
 
-    func test_() {
+    func test_() async throws {
+        let store = try await InAppStore(
+            productsPlist: productsPlist,
+            onStateChange: { _ in }
+        )
+        let session = try SKTestSession(configurationFileNamed: "Configuration")
+        session.disableDialogs
+        session.clearTransactions()
+        let state = await store.state
+        XCTAssertEqual(state.available.autoRenewables.count, 5)
         
     }
 
